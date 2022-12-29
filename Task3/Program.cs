2d587e4406ddc88a13a3ef11145fb9533f8f97d2
Julia Gomozova <sunrain@yandex.ru>
2022-12-29 22:57:59 +0300

    add Task3 and Task4

---
+++ Task3/Program.cs
@@ -0,0 +1,96 @@
+﻿/*
+Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18
+*/
+// Запросить у пользователя целое число
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+    return result;
+}
+
+// Инициализировать рандомный двумерный массив 
+int[,] InitMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rnd.Next(1, 10);
+        }
+    } 
+    return matrix;
+}
+
+// Вывести двумерный массив на консоль
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] MultiplyMatrix(int[,] a, int[,] b)
+{
+    int[,] c = new int[a.GetLength(0), b.GetLength(1)];
+
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < b.GetLength(1); j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < a.GetLength(1); k++)
+            {
+                sum += a[i,k] * b[k,j];    
+            }
+            c[i,j] = sum;
+        }  
+    }
+    return c;
+}
+
+
+int rows1 = GetNumber("Введите количество строк матрицы А:");
+int columns1 = GetNumber("Введите количество столбцов матрицы А");
+int rows2 = columns1;
+int columns2 = GetNumber("Введите количество столбцов матрицы B");
+
+int[,] a = InitMatrix(rows1, columns1);
+int[,] b = InitMatrix(rows2, columns2);
+
+Console.WriteLine();
+Console.WriteLine("Матрица А:");
+PrintMatrix(a);
+
+Console.WriteLine("Матрица B:");
+PrintMatrix(b);
+
+Console.WriteLine();
+Console.WriteLine("A*B:");
+PrintMatrix(MultiplyMatrix(a, b));
---
+++ Task4/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1)
+*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+    return result;
+}
+
+int[,,] Init3DMatrix(int l, int m, int n)
+{
+    int len = 90;
+    int[] nums = new int[len];
+    int[,,] matrix = new int[l, m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < 90; i++)
+    {
+        nums[i] = i + 10;
+    }
+    for (int i = 0; i < l; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            for (int k = 0; k < n; k++)
+            {
+                int pos = rnd.Next(0, len);
+                matrix[i, j, k] = nums[pos];
+                len--;
+                Console.Write($"{matrix[i,j,k]}({i},{j},{k}) ");
+                for (int x = pos; x < len; x++)
+                    nums[x] = nums[x + 1];
+            }
+            
+        }
+        
+    }
+    return matrix;
+}
+
+
+int l = GetNumber("Введите число 1 измерения матрицы:");
+int m = GetNumber("Введите число 2 измерения матрицы:");
+int n = GetNumber("Введите число 3 измерения матрицы:");
+if(l*m*n > 90) 
+    Console.WriteLine("Слишком большие размеры массива, для заполнения неповторяющимися двузначными числами");
+else
+    Init3DMatrix(l, m, n);
