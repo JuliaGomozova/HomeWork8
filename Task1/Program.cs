0a8c783d18e83a0ba1869187c0770ab8c87c71ac
Julia Gomozova <sunrain@yandex.ru>
2022-12-29 16:12:47 +0300

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,95 @@
+﻿/*
+Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+    return result;
+}
+
+// Инициализировать рандомный двумерный массив 
+int[,] InitMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rnd.Next(1, 10);
+        }
+    } 
+    return matrix;
+}
+
+// Вывести двумерный массив на консоль
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+}
+
+void SortRowsFromMax(int[,] matrix)
+{
+    
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1)-1; k++)
+            {
+                if(matrix[i,k] < matrix[i,k+1])
+                {
+                    int tmp = matrix[i,k];
+                    matrix[i,k] = matrix[i,k+1];
+                    matrix[i,k+1] = tmp;
+                }
+
+            }
+        }
+    }
+}
+
+int rows = GetNumber("Зададим двумерный массив, для этого введите количество строк:");
+int columns = GetNumber("Количество столбцов:");
+
+Console.WriteLine();
+
+int[,] matrix = InitMatrix(rows, columns); 
+
+PrintMatrix(matrix);
+Console.WriteLine();
+
+SortRowsFromMax(matrix);
+PrintMatrix(matrix);
